af94825490abc02bf912dc5b4ae699d7ecffba42
Daniel Christopher Roffman <daniel@upstartpark.com>
2013-03-16 15:47:22 +0800

    Created a rails app and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appstartpark::Application.config.secret_token = '83a53e12e8363c7120cefbefb631c4b8aa36073a6c3ca98b51c182ca343f47c45ffef403280f314ff5fcaea2e1b36aaa12ce7200d9376c0b009e6e66e2226adf'
